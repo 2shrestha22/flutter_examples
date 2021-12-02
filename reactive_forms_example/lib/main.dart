d51089fc75ac44fc8f93d46db7babc6ec9563909
Sangam Shrestha <2shrestha22@gmail.com>
2021-12-02 10:20:52 +0545

    wip reactive form example

---
+++ reactive_forms_example/lib/main.dart
@@ -0,0 +1,96 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:reactive_forms/reactive_forms.dart';
+
+final form = FormGroup({
+  'personal': FormGroup({
+    'name': FormControl<String>(validators: [Validators.required]),
+    'email': FormControl<String>(
+      validators: [Validators.required, Validators.email],
+    ),
+  }),
+  'phone': FormGroup({
+    'phoneNumber': FormControl<String>(
+      validators: [Validators.required, Validators.number],
+    ),
+    'countryIso': FormControl<String>(validators: [Validators.required]),
+  }),
+  'address': FormGroup({
+    'street': FormControl<String>(validators: [Validators.required]),
+    'city': FormControl<String>(validators: [Validators.required]),
+    'zip': FormControl<String>(validators: [Validators.required]),
+  }),
+});
+
+final familyMemberForm = FormGroup({
+  'familyMember': FormArray([form])
+});
+
+const sb = SizedBox(height: 20);
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        inputDecorationTheme: InputDecorationTheme(
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(8)),
+        ),
+      ),
+      home: const ReactiveFormsExample(),
+    );
+  }
+}
+
+class ReactiveFormsExample extends StatefulWidget {
+  const ReactiveFormsExample({Key? key}) : super(key: key);
+
+  @override
+  State<ReactiveFormsExample> createState() => _ReactiveFormsExampleState();
+}
+
+class _ReactiveFormsExampleState extends State<ReactiveFormsExample> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Reactive Forms'),
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16),
+        child: ReactiveForm(
+          formGroup: familyMemberForm,
+          child: Column(
+            children: <Widget>[
+              ReactiveTextField(
+                formControlName: 'familyMember[0].personal.name',
+                decoration: const InputDecoration(labelText: 'Name'),
+              ),
+              sb,
+              ReactiveTextField(
+                formControlName: 'familyMember.personal.email',
+                decoration: const InputDecoration(labelText: 'Email'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {});
+                },
+                child: const Text('Save'),
+              ),
+              Text(jsonEncode(form.value)),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
